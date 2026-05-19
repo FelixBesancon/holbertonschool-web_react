57e059447aa46bd2fc9a141b6e742ad04ed7e0ea
FelixBesancon <f.besancon@hotmail.fr>
2026-05-19 10:13:31 +0200

    Add TypeScript setup with Webpack configuration, package.json, and tsconfig.json
    - Created package.json with development dependencies for TypeScript, Webpack, and Jest.
    - Added tsconfig.json for TypeScript compiler options.
    - Implemented webpack.config.js for module bundling and development server setup.

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -16,3 +16,61 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printMyTeacher: printTeacherFunction = printTeacher;
+console.log(printMyTeacher(teacher3.firstName, teacher3.lastName));
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework (): string;
+  displayName (): string;
+}
+
+interface StudentClassConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor (firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework (): string {
+    return "Currently working";
+  }
+
+  displayName (): string {
+    return this.firstName;
+  }
+}
+
+const MyStudentClass: StudentClassConstructor = StudentClass;
+const student = new MyStudentClass("Jason", "Bourne");
+console.log(student.displayName());
